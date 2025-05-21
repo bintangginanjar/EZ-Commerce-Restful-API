9c8cb4bf9ae220d32d9283a6c03687c163fc4996
bintangginanjar <bintang.ginanjar@gmail.com>
2025-05-21 10:09:38 +0700

    core : Change configuration to dotenv

--- src/main/java/rest/api/ezcommerce/security/SecurityConstants.java
+++ src/main/java/rest/api/ezcommerce/security/SecurityConstants.java
@@ -20,10 +20,4 @@ public class SecurityConstants {
         return jwtSecret;
     }
 
-    /*
-    public static final int JWTexpiration = 3600000;
-
-    public static final String JWTsecret = "Th3k0p4tspringboot";
-    */
-
 }
